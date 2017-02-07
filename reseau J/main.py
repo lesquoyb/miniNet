fc541fa70b705dc5d135e07783e14d5ac0eb4d7b
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2017-02-07 02:07:24 +0100

    tmp croisé

--- reseau J/main.py
+++ reseau J/main.py
@@ -19,8 +19,25 @@ def main():
     data, classes = parseFile("iris.data.txt")
     print(data)
     print(classes)
+    nBlocks = 5
+    size = len(data)//nBlocks
+    bData = []
+    bClasses = []
+    for i in range(nBlocks):
+        bData += [data[i*size: (i+1) * size]]
+        bClasses += [classes[i*size: (i+1) * size]]
+
     network = Network()
-    network.learn([[0,0,0,0],[0,0,0,0]], [[1,0,0],[0,0,1]])
+
+    error = 0
+    for i in range(nBlocks):
+        tData = bData[i]
+        tClasses = bClasses[i]
+        data = [ b for j,b in enumerate(bData) if j != i]
+        classes = [ b for j,b in enumerate(bClasses) if j != i]
+        network.learn(data, classes)
+        error += network.error_rate(tData, tClasses)
+    print(str(error/(len(data)*nBlocks) + "% d'erreur"))
 
 
 if __name__ == "__main__":
